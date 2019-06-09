253209c8d8217e8bdb06aa9a6d971c86a908a6d7
Victor <victor.herrera.sc@itszapopan.edu.mx>
2019-06-09 09:47:49 -0500

    Initial Commit

---
+++ src/main/java/com/victorherrera/onlinestoremvcexercise/OnlineStoreMvcExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.victorherrera.onlinestoremvcexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreMvcExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreMvcExerciseApplication.class, args);
+    }
+
+}
