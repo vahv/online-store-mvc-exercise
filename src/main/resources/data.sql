1114ec10471f2177f8d8f2a0b89e740a93d7ae87
Victor <victor.herrera.sc@itszapopan.edu.mx>
2019-06-09 20:57:49 -0500

    Added Data initialization file.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO laptop (model, brand , price) values('80XS','Lenovo',8499.00);
+INSERT INTO laptop (model, brand , price) values('240 G5','HP', 7699.00);
+INSERT INTO laptop (model, brand , price) values('A315-41-R4RE','Acer', 10599.00);
+INSERT INTO laptop (model, brand , price) values('A441NA-GA313T','Asus', 5699.00);
+INSERT INTO laptop (model, brand , price) values('330-15IKB','Lenovo', 7989.00);
+INSERT INTO laptop (model, brand , price) values('AN515-53-52FA','Acer', 15999.00);
